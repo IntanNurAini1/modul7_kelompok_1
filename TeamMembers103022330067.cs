0498650474cc47224e67d7b722790549f6aaf203
IntanNurAini <intan.nuraini2005@gmail.com>
2025-04-08 08:16:58 +0700

    Menambahkan class TeamMembers103022330067

---
+++ TeamMembers103022330067.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class TeamMembers103022330067
+{
+    class Members
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string nim { get; set; }
+    }
+    class team
+    {
+        public List<Members> members { get; set; }
+    }
+
+
+}
