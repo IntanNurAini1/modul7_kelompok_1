cae3018de1df51cf075d9d6d2cdd8e9c79323c58
xxraihann <raihanananda1980@gmail.com>
2025-04-08 07:28:19 +0700

    Menambahkan class DataMahasiswa103022330075

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using modul7_kelompok_1;
+using System;
+
+class PProgram
+{
+    static void main(String[] args)
+    {
+        var Mahasiswa = new DataMahasiswa103022330075();
+        Mahasiswa.ReadJSON();
+    }
+}
